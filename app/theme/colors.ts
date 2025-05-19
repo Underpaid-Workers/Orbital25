d198f5adc819eadc66a748ede9c8070ec5dafb6d
rongkangw <rongkangw@gmail.com>
2025-05-19 15:49:05 +0800

    Updated assets, colors, tabTemplate
    - Added LoginImage, AppIcon to assets
    - Added basic color scheme (import )
    - Renamed tabTemplate.tsx
    - Some automatic package.json changes

---
+++ app/theme/colors.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  text: '#000000',
+  background: '#FFFFFF',
+  icon: '#000000',
+  tab:'#475756',
+  tabSelected:"#000000",
+  tabBar: '#60BB7E'
+};
+
+//use this template to enable light,dark mode colors
+
+// export const colors = {
+//   light: {
+//     ...add colors here
+//   },
+//   dark: {
+//     ...add colors here
+//   },
+// };
+
+export default colors
